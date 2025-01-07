a4cf67e1c825be39e095c66c483732855755a43e
Shefali <shefalimurarka19@gmail.com>
2025-01-07 12:58:10 +0530

    VPC Setup

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,21 +1,21 @@
 output "vpc_id" {
-  value = "aws_vpc.main.id"
+  value = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  value = "aws_subnet.public.*.id"
+  value = aws_subnet.public.*.id
 }
 
 output "app_subnet_ids" {
-  value = "aws_subnet.app.*.id"
+  value = aws_subnet.app.*.id
 }
 
 output "web_subnet_ids" {
-  value = "aws_subnet.web.*.id"
+  value = aws_subnet.web.*.id
 }
 
 output "db_subnet_ids" {
-  value = "aws_subnet.db.*.id"
+  value = aws_subnet.db.*.id
 }
 
 output "subnets" {
@@ -25,4 +25,12 @@ output "subnets" {
       "db"     = aws_subnet.db.*.id
       "public" = aws_subnet.public.*.id
     })
+}
+
+output "vpc_cidr" {
+  value = var.cidr
+}
+
+output "default_vpc_cidr" {
+  value = var.default_vpc_cidr
 }
