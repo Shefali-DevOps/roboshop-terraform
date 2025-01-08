e7e75b2f06de61aceb0d0f60de9744bd6402b4ac
Shefali <shefalimurarka19@gmail.com>
2025-01-08 15:54:21 +0530

    Added public Http to redirect to HTTPS

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -131,7 +131,7 @@ resource "aws_lb_target_group" "main" {
 resource "aws_lb_listener" "internal-http" {
   count     =   var.internal ? 1 : 0
   load_balancer_arn = aws_lb.main.arn
-  port = 80
+  port = "80"
   protocol = "HTTP"
 
   default_action {
@@ -143,7 +143,7 @@ resource "aws_lb_listener" "internal-http" {
 resource "aws_lb_listener" "public-https" {
   count     =   var.internal ? 0 : 1
   load_balancer_arn = aws_lb.main.arn
-  port = 443
+  port = "443"
   protocol = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.acm_https_arn
@@ -151,6 +151,23 @@ resource "aws_lb_listener" "public-https" {
   default_action {
     type = "forward"
     target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_listener" "public-http" {
+  count     =   var.internal ? 0 : 1
+  load_balancer_arn = aws_lb.main.arn
+  port = "80"
+  protocol = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTPPS_301"
+    }
   }
 }
 
